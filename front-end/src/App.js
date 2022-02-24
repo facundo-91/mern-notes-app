6c79c264e981e3f4500245ed42b42e8073efab3e
Facundo Benitez <facundob91@gmail.com>
2022-02-24 15:57:58 -0300

    add toast notification when adding and deleting todos

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from 'react'
-import todoService from './services/todos'
-import './App.css';
+import { ToastContainer, toast, Slide } from 'react-toastify';
 import Header from './components/Header';
 import TodosContainer from './components/TodosContainer';
+import todoService from './services/todos'
+import 'react-toastify/dist/ReactToastify.css';
+import './App.css';
 
 function App() {
 	const [todos, setTodos] = useState([]);
@@ -16,20 +18,54 @@ function App() {
 	}, []);
 
 	const addTodo = (todoObject) => {
-		todoService
+		try {
+			todoService
 			.create(todoObject)
 			.then(returnedTodo => {
 				setTodos(todos.concat(returnedTodo))
+				toast.success('New todo added!', {
+					theme: "colored"
+				})
 			})
+		} catch (error) {
+			console.error(error)
+			toast.error('Error adding todo', {
+					theme: "colored"
+				})
+		}
 	}
 
 	const removeTodo = id => {
-		todoService.remove(id)
-		setTodos(prevState => prevState.filter(todo => todo.id !== id));
+		try {
+			todoService.remove(id)
+			setTodos(prevState => prevState.filter(todo => todo.id !== id));
+			toast.info('Todo Removed!', {
+					theme: "colored"
+				})
+		} catch (error) {
+			console.error(error)
+			toast.error('Error removing todo', {
+					theme: "colored"
+				})
+		}
 	}
 
 	const removeCompletedTodos = () => {
-		todos.map(todo => (todo.completed === false) ? todo : removeTodo(todo.id))
+		const removeSingleTodo = id => {
+			todoService.remove(id)
+			setTodos(prevState => prevState.filter(todo => todo.id !== id))
+		}
+		try {
+			todos.map(todo => (todo.completed === false) ? todo : removeSingleTodo(todo.id))
+			toast.info('All completed todos removed!', {
+					theme: "colored"
+				})
+		} catch (error) {
+			console.error(error)
+			toast.error('Error deleting completed todos', {
+					theme: "colored"
+				})
+		}
 	}
 
 	const updateTodo = (id, todoContent, todoCompleted) => {
@@ -37,11 +73,15 @@ function App() {
 			content: todoContent,
 			completed: todoCompleted
 		}
-		todoService
+		try {
+			todoService
 			.update(id, updatedTodo)
 			.then(returnedTodo => {
 				setTodos(prevState => prevState.map(todo => (todo.id !== returnedTodo.id) ? todo : returnedTodo))
 			})
+		} catch (error) {
+			console.error(error)
+		}
 	}
 
 	const completeTodos = check => {
@@ -52,6 +92,7 @@ function App() {
 		<div className="App">
 			<Header todos={todos} createTodo={addTodo} completeTodos={completeTodos} />
 			<TodosContainer todos={todos} deleteTodo={removeTodo} deleteCompletedTodos={removeCompletedTodos} editTodo={updateTodo} />
+			<ToastContainer autoClose={3000} transition={Slide} pauseOnFocusLoss={false} pauseOnHover={false} hideProgressBar={true} />
 		</div>
 	);
 }
